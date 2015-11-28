8ee9f6046c02626c833428688c46668e20d4d0a1
nikhilc@bsf.io <nikhilc@bsf.io>
2015-11-28 18:30:13 +0530

    some styling for the pre tags

--- error-logger.php
+++ error-logger.php
@@ -8,7 +8,7 @@
 function el( $var ) {
 	get_style(); 
 	echo "<pre class='el_pre'><xmp>";
-	if ( is_array( $var ) ) {
+	if ( is_array( $var ) || is_object( $var ) ) {
 		print_r( $var );
 	} else {
 		echo $var;
@@ -28,6 +28,23 @@ function get_style() {
 	<style type="text/css">
 		.el_pre {
 			text-align: left;
+			margin: 30px 15px;
+		    padding: 1em;
+		    border: 0px;
+		    outline: 0px;
+		    font-size: 14px;
+		    font-family: monospace;
+		    vertical-align: baseline;
+		    max-width: 100%;
+		    overflow: auto;
+		    color: rgb(248,248,242);
+		    direction: ltr;
+		    word-spacing: normal;
+		    line-height: 1.5;
+		    border-radius: 0.3em;
+		    word-wrap: normal;
+		    letter-spacing: 0.266667px;
+		    background: rgb(61,69,75);
 		}
 	</style>
 	<?php
