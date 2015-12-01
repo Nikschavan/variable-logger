d0309c380ff48de524da98b5e4d77764bddd7178
nikhilc@bsf.io <nikhilc@bsf.io>
2015-12-01 22:07:58 +0530

    combined el_dump and el functions

--- error-logger.php
+++ error-logger.php
@@ -5,21 +5,20 @@
  * Version: 0.1-alpha
  * Author: Nikhil Chavan
  */
-function el( $var ) {
+function el( $var, $dump = 0 ) {
 	get_style(); 
 	echo "<pre class='el_pre'><xmp>";
-	if ( is_array( $var ) || is_object( $var ) ) {
-		print_r( $var );
+	if ( true == $dump ) {
+		var_dump( $var );
 	} else {
-		echo $var;
-	}
-	echo "</xmp></pre>";
-}
 
-function el_dump( $var ) {
-	get_style();
-	echo "<pre class='el_pre'><xmp>";
-	var_dump( $var );
+		if ( is_array( $var ) || is_object( $var ) ) {
+			print_r( $var );
+		} else {
+			echo $var;
+		}
+
+	}
 	echo "</xmp></pre>";
 }
 
@@ -29,22 +28,22 @@ function get_style() {
 		.el_pre {
 			text-align: left;
 			margin: 30px 15px;
-		    padding: 1em;
-		    border: 0px;
-		    outline: 0px;
-		    font-size: 14px;
-		    font-family: monospace;
-		    vertical-align: baseline;
-		    max-width: 100%;
-		    overflow: auto;
-		    color: rgb(248,248,242);
-		    direction: ltr;
-		    word-spacing: normal;
-		    line-height: 1.5;
-		    border-radius: 0.3em;
-		    word-wrap: normal;
-		    letter-spacing: 0.266667px;
-		    background: rgb(61,69,75);
+			padding: 1em;
+			border: 0px;
+			outline: 0px;
+			font-size: 14px;
+			font-family: monospace;
+			vertical-align: baseline;
+			max-width: 100%;
+			overflow: auto;
+			color: rgb(248,248,242);
+			direction: ltr;
+			word-spacing: normal;
+			line-height: 1.5;
+			border-radius: 0.3em;
+			word-wrap: normal;
+			letter-spacing: 0.266667px;
+			background: rgb(61,69,75);
 		}
 	</style>
 	<?php
