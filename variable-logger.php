05687bca8e0d50381436f83823351007f10d60cd
Nikhil <email@nikhilchavan.com>
2018-07-02 14:45:38 +0530

    use dumper instead of custom function

--- variable-logger.php
+++ variable-logger.php
@@ -6,6 +6,8 @@
  * Author: Nikhil Chavan
  */
 
+require_once 'vendor/autoload.php';
+
 /**
  * Replacement for print_r & var_dump.
  *
@@ -13,44 +15,5 @@
  * @param bool $dump. (default: false)
  */
 function vl( $var, $dump = 0 ) {
-	?>
-
-	<style type="text/css">
-		.vl_pre {
-			text-align: left;
-			margin: 30px 15px;
-			padding: 1em;
-			border: 0px;
-			outline: 0px;
-			font-size: 14px;
-			font-family: monospace;
-			vertical-align: baseline;
-			max-width: 100%;
-			overflow: auto;
-			color: rgb(248,248,242);
-			direction: ltr;
-			word-spacing: normal;
-			line-height: 1.5;
-			border-radius: 0.3em;
-			word-wrap: normal;
-			letter-spacing: 0.266667px;
-			background: rgb(61,69,75);
-		}
-	</style>
-
-	<?php
-	
-	echo "<pre class='vl_pre'><xmp>";
-	if ( true == $dump ) {
-		var_dump( $var );
-	} else {
-
-		if ( is_array( $var ) || is_object( $var ) ) {
-			print_r( $var );
-		} else {
-			echo $var;
-		}
-
-	}
-	echo "</xmp></pre>";
+	dump( $var );
 }
